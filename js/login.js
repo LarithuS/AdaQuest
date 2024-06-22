bfdee520c51138f132d582787cfd98650aca0b8d
Ada Gutan <adagutan@gmail.com>
2024-06-23 00:05:05 +0300

    whats going on

--- js/login.js
+++ js/login.js
@@ -1,10 +1,16 @@
-function login(){
+var form = document.getElementById("loginform");
+    function handleForm(event) { event.preventDefault(); } 
+    form.addEventListener('submit', handleForm);
+
+function login() {
   document.getElementById("Error").innerHTML = "";
-  
+
   const mail = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  if(mail != null || password != null){
-    document.getElementById("Error").innerHTML = "Idk how to do that yet <3";
+  if (mail !== null && password !== "") {
+      document.getElementById("Error").innerHTML = "Idk how to do that yet <3";
+  } else {
+      document.getElementById("Error").innerHTML = "Please enter both email and password.";
   }
 }
