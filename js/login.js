21684d7ab8f519ce2e8eff8b1cb18993e098626f
Ada Gutan <adagutan@gmail.com>
2024-06-24 15:33:13 +0300

    tolga abi halletti

--- js/login.js
+++ js/login.js
@@ -1,16 +1,26 @@
-var form = document.getElementById("loginform");
-    function handleForm(event) { event.preventDefault(); } 
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById("loginform");
+    
+    function handleForm(event) { 
+        event.preventDefault(); 
+    } 
+
     form.addEventListener('submit', handleForm);
 
-function login() {
-  document.getElementById("Error").innerHTML = "";
+    function login() {
+        document.getElementById("Error").innerHTML = "";
 
-  const mail = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+        const mail = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
 
-  if (mail !== null && password !== "") {
-      document.getElementById("Error").innerHTML = "Idk how to do that yet <3";
-  } else {
-      document.getElementById("Error").innerHTML = "Please enter both email and password.";
-  }
-}
+        if (mail !== "" && password !== "") {
+            document.getElementById("Error").innerHTML = "Idk how to do that yet <3";
+        } else {
+            document.getElementById("Error").innerHTML = "Please enter both email and password.";
+        }
+    }
+
+    document.getElementById("login").addEventListener("click", login);
+});
